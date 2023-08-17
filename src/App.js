69961ed34358a2b35df79eef14a25d0a43a6544e
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-17 10:27:28 +0000

    top bottons static

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
+	return (
+		<div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
     </div>
-  );
+	);
 }
 
 export default App;
